8ca8bc90d120e2ecd7ddc7659534e18c67fe9527
PETERSHEN0327 <qiwenshen0327@gmail.com>
2024-11-14 03:00:23 +0800

    Update code with new features/fixes

--- models/BorrowRecord.js
+++ models/BorrowRecord.js
@@ -4,7 +4,7 @@ const borrowRecordSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   bookId: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
   borrowDate: { type: Date, default: Date.now },
-  returnDate: { type: Date },
+  returnDate: { type: Date }
 });
 
 module.exports = mongoose.model('BorrowRecord', borrowRecordSchema);
