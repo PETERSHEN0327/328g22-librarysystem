44be5e076efcc0519094b29636523e1e9848f69a
PETERSHEN0327 <qiwenshen0327@gmail.com>
2024-11-14 02:32:28 +0800

    Update code with new features/fixes

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ app.get('/', (req, res) => {
 
 // 路由配置
 app.use('/', userRoutes); // 将 userRoutes 挂载到根路径
-app.use('/books', bookRoutes);
-app.use('/borrow', borrowRoutes);
+app.use('/api/books', bookRoutes);
+app.use('/api/borrow', borrowRoutes);
 
 // 启动服务器
 app.listen(PORT, () => {
